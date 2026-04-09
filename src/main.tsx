5fa83e23ae0f42bc598adaf4e03bbd55f9f101d2
shreyoshi2409 <chakrabortyshreyoshi721@gmail.com>
2026-04-09 11:48:04 +0530

    Fix GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Handle GitHub Pages redirect
+const redirect = new URLSearchParams(window.location.search).get('redirect');
+if (redirect) {
+  window.history.replaceState(null, '', redirect);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
